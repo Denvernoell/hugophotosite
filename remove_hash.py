bd9f0c01d82c755598ee92334f947f6efd3f1f63
denvernoell <denvernoell@gmail.com>
2021-03-19 12:59:40 -0700

    removed hashes

--- remove_hash.py
+++ remove_hash.py
@@ -6,10 +6,11 @@ os.chdir('static/img')
 for decade in range(10, 70, 10):
     mypath = f"./{decade}/"
     for f in os.listdir(mypath):
+        print(f)
 
         if f.find('#') != -1:
             print(os.path.join(mypath, str(f)))
-            os.replace(os.path.join(mypath, f), os.path.join(
+            os.rename(os.path.join(mypath, f), os.path.join(
                 mypath, f.replace('#', '')))
     # print('\n'*10)
         # if f.find('#') != -1:
