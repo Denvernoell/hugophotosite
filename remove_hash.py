ce16040fa123aa4e9882fdb6e4892e4f15ac8829
denvernoell <denvernoell@gmail.com>
2021-03-19 10:28:46 -0700

    removed hashes

--- remove_hash.py
+++ remove_hash.py
@@ -7,15 +7,10 @@ for decade in range(10, 70, 10):
     mypath = f"./{decade}/"
     for f in os.listdir(mypath):
 
-        # try:
-        #     # os.join(decade,f)
-        #     print(os.join(decade, f.replace('#', '')))
-        #     os.rename(os.join(decade, f), os.join(decade, f.replace('#', '')))
-
-        # except:
-        #     pass
         if f.find('#') != -1:
             print(os.path.join(mypath, str(f)))
-            os.rename(os.path.join(mypath, f), os.path.join(
-                mypath, f.replace('#', '')))
-        #
+            # os.rename(os.path.join(mypath, f), os.path.join(
+            #     mypath, f.replace('#', '')))
+    # print('\n'*10)
+        # if f.find('#') != -1:
+        #     print(os.path.join(mypath, str(f)))
