21c6f4e219dd6a75553e6b9405a985d70bd5c669
denvernoell <denvernoell@gmail.com>
2021-03-19 10:22:57 -0700

    removed hashes

--- remove_hash.py
+++ remove_hash.py
@@ -4,15 +4,18 @@ import os
 
 os.chdir('static/img')
 for decade in range(10, 70, 10):
-    for f in os.listdir(f"./{decade}/"):
+    mypath = f"./{decade}/"
+    for f in os.listdir(mypath):
 
-        try:
-            # os.join(decade,f)
-            print(os.join(decade, f.replace('#', '')))
-            os.rename(os.join(decade, f), os.join(decade, f.replace('#', '')))
+        # try:
+        #     # os.join(decade,f)
+        #     print(os.join(decade, f.replace('#', '')))
+        #     os.rename(os.join(decade, f), os.join(decade, f.replace('#', '')))
 
-        except:
-            pass
-        # if f.find('#') != -1:
-        #     print(f)
+        # except:
+        #     pass
+        if f.find('#') != -1:
+            print(os.path.join(mypath, str(f)))
+            os.rename(os.path.join(mypath, f), os.path.join(
+                mypath, f.replace('#', '')))
         #
