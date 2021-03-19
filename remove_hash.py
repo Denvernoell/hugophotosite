d093fd897f4c1557dfe2969c9ce8156d36370596
denvernoell <denvernoell@gmail.com>
2021-03-19 10:40:36 -0700

    removed hashes

--- remove_hash.py
+++ remove_hash.py
@@ -9,8 +9,8 @@ for decade in range(10, 70, 10):
 
         if f.find('#') != -1:
             print(os.path.join(mypath, str(f)))
-            # os.rename(os.path.join(mypath, f), os.path.join(
-            #     mypath, f.replace('#', '')))
+            os.replace(os.path.join(mypath, f), os.path.join(
+                mypath, f.replace('#', '')))
     # print('\n'*10)
         # if f.find('#') != -1:
         #     print(os.path.join(mypath, str(f)))
